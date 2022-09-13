454974f5d951aa2fb1db36cf894349cc55d61231
baariq <baariq.fairuuz@dataon.com>
2022-09-13 14:57:59 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const whitelist = ["http://localhost:3000"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// ? remark if development
+// app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World!",
+    requestBody: req.body,
+  });
+});
+
+const PORT = 8100;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
